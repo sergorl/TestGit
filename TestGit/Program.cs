733f4bd3b1cf9b0dc46101375e9f1511de96cf73
sergorl <serg.neworl@gmail.com>
2019-11-24 00:16:48 +0300

    edited Program

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,7 +6,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello Serg!");
         }
     }
 }
